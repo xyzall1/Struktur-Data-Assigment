2ae23e242827121e1adeca4dbcc3db1d6618d73e
xyzall1 <rosa.aliana316@gmail.com>
2024-06-10 13:59:01 +0700

    bismillah

---
+++ POST TEST/2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long factorial (int n); {
+    long long hasil = 1;
+    iterasi (n, hasil);
+    return hasil;
+
+int main() {
+    int bilangan; 
+}
+cout << "Masukin bilangan" << endl;
+cin << "hasil" << endl;
+
+long long hasil = factorial (bilangan);
+cout << "Faktorial dari " << bilangan << " adalah: " << hasil << endl;
+
+}
+
---
+++ POST TEST/3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#using namespace std;
+
+int main() {
+    int = 5;
+    int data [n] = {5, 1, 2, 8};
+    int cari = 2;
+    bool ketemu = false;
+    int i ;
+
+    for (i = 0; i < n: i++) {
+        if (data [i] == cari) {
+            ketemu = true;
+            break;
+        }
+    }
+
+    cout << "\t Pencarian sequential search"\n << endl;
+    cout << "data : {1, 2, 3, 4, 5, 5}" << endl;
+        if (ketemu) {
+    cout << \n "angka "  << cari << "ketemu di indeks ke-" << i << endl;
+
+    } else {
+    cout << cari << "ga ketemu" endl;
+
+    }
+    return 0;
+}
